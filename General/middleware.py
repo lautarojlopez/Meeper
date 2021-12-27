47e0313e60be61b7328a133da562e7e846e78f9e
Lauty <lopezlautarojoaquin1@gmail.com>
2021-12-27 19:07:49 -0300

    Commit

--- General/middleware.py
+++ General/middleware.py
@@ -6,7 +6,6 @@ def a_quien_seguir(get_response):
         if request.user.is_authenticated:
             user_ids = Relacion.objects.filter(from_user=request.user).values_list('to_user_id', flat=True)
             request.no_seguidos = User.objects.all().exclude(id__in=user_ids).exclude(id=request.user.id).order_by('?')[:3]
-            print(request.no_seguidos)
         response = get_response(request)
         # Código que se ejecutará después del llamado a la vista.
         return response
